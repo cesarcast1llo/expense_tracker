f0c56079f63d141a5f8cfa0ea912b612416cde94
cesarcast1llo <cesarcast1llo@yahoo.com>
2020-03-24 00:13:47 -0700

    testing for dotenv

---
+++ client/build/precache-manifest.6d99308e1c3dbdc3794b6b4ee93cb62d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0773831dc743381fdc35bca10e113358",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66a0d8f848f978088df6",
+    "url": "/static/css/main.708a4d9b.chunk.css"
+  },
+  {
+    "revision": "fcb1029d74144164bee3",
+    "url": "/static/js/2.9632fcdd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9632fcdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66a0d8f848f978088df6",
+    "url": "/static/js/main.e07fbe72.chunk.js"
+  },
+  {
+    "revision": "51a518e45330f669e104",
+    "url": "/static/js/runtime-main.51eec7e2.js"
+  }
+]);
